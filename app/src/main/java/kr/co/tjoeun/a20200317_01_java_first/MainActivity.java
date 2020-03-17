3339390ece95b0eb1a73950bc789874240e1be03
liemeon <44192664+liemeon@users.noreply.github.com>
2020-03-17 15:03:06 +0900

    바인딩작업

--- app/src/main/java/kr/co/tjoeun/a20200317_01_java_first/MainActivity.java
+++ app/src/main/java/kr/co/tjoeun/a20200317_01_java_first/MainActivity.java
@@ -1,14 +1,26 @@
 package kr.co.tjoeun.a20200317_01_java_first;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import org.w3c.dom.Text;
+
+import kr.co.tjoeun.a20200317_01_java_first.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding = null;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main);
+
+
     }
 }
